48368a994e61623d6955b9c997087b921e22a1f1
SoeMyatMon <soemyatmon476@gmail.com>
2025-04-27 11:05:56 +0630

    fixed layout_footer

--- react_sample/src/components/Footer.jsx
+++ react_sample/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="mx-5 bg-black text-white py-2 text-center">
-      Developed by SoeMyatMon @ 2025
+    <footer className="mx-5 bg-black text-white py-2 text-center mt-auto">
+      Developed by SoeMyatMon @ {currentYear}
     </footer>
   );
 };
